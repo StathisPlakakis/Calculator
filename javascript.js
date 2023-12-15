2246ea9acf05e5514fcc7252343e2acf56c0fd8e
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-15 17:56:56 +0200

    Make the event listener display only numbers

--- javascript.js
+++ javascript.js
@@ -2,6 +2,14 @@ const buttons = document.querySelectorAll('.button');
 const operations = document.querySelector('.operations');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        operations.textContent = button.textContent;
+        if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(button.textContent)) {
+            if (operations.textContent === '0') {
+                operations.textContent = button.textContent;
+            }else{
+                operations.textContent += button.textContent;          
+            }
+        }
+
+        
     })
 })
