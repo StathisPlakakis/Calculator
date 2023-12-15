b1ee448e5cbaf460e02dfaaf890127efbf005ac8
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-15 18:48:35 +0200

    Delede redundant css

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 const buttons = document.querySelectorAll('.button');
-const operations = document.querySelector('.operations');
+const display = document.querySelector('.display');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(button.textContent)) {
-            if (operations.textContent === '0') {
-                operations.textContent = button.textContent;
+            if (display.textContent === '0') {
+                display.textContent = button.textContent;
             }else{
-                operations.textContent += button.textContent;          
+                display.textContent += button.textContent;          
             }
         }
 
